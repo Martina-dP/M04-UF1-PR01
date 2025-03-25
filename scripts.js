82b3e95abd946f4810bdc28a7b5293c701dbc3f2
Martina del Pino <martudelpino@gmail.com>
2025-03-25 22:46:52 +0100

    terminado

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function changeColor() {
+    const contenedor = document.querySelector(".container-search");
+    contenedor.style.backgroundColor = "#0057B8" 
+
+    console.log("Contenedor formulario cambiado a azul");
+    console.log(contenedor.style.backgroundColor);
+}
